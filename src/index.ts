cc4422c5022bb580371bc29c316f94b5dc88d108
Łukasz Lalik <lukasz.lalik@gmail.com>
2019-12-09 08:34:06 +0100

    Remove custom style.css

--- src/index.ts
+++ src/index.ts
@@ -1,211 +1,100 @@
-import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Renderer from "./engine/Renderer";
 import Transform from "./engine/Transform";
 import Vector2D from "./engine/Vector2D";
 import Box from "./renderables/Box";
 import Circle from "./renderables/Circle";
 import RowLayout from "./renderables/RowLayout";
-import {MacLayer} from "./lte/MacLayer";
-import {LogicalChannelType, TransportChannelType} from "./lte/ChannelType";
+import {MacLayer} from "./model/lte/MacLayer";
+import {LogicalChannelType, TransportChannelType} from "./model/lte/ChannelType";
 import {SlideAnimation} from "./animators/SlideAnimation";
-import {TransmitPacketDownLink} from "./lte/Action";
+import {TransmitPacketDownLink} from "./model/lte/Action";
+import {Simulation} from "./view/Simulation";
+import {SimulationController} from "./controller/SimulationController";
 
 const canvas = <HTMLCanvasElement>document.getElementById('simulation');
 const renderer = new Renderer(canvas.getContext('2d'));
 const startSimulationButton = <HTMLButtonElement>document.getElementById('start-simulation');
 
-startSimulationButton.addEventListener('click', startSimulation);
-
 const macLayer = new MacLayer();
-const PCCH = new Circle(15, '0'),
-  BCCH = new Circle(15, '0'),
-  CCCH = new Circle(15, '0'),
-  DCCH = new Circle(15, '0'),
-  DTCH = new Circle(15, '0'),
-  MCCH = new Circle(15, '0'),
-  MTCH = new Circle(15, '0'),
-  PCH = new Circle(15, '0'),
-  BCH = new Circle(15, '0'),
-  DL_SCH = new Circle(15, '0'),
-  MCH = new Circle(15, '0');
 
+const simulation = new Simulation();
 
-function startSimulation() {
-  macLayer.setChannelsPriorities([
-    LogicalChannelType.PCCH,
-    LogicalChannelType.BCCH,
-    LogicalChannelType.CCCH,
-    LogicalChannelType.DCCH,
-    LogicalChannelType.DTCH,
-    LogicalChannelType.MCCH,
-    LogicalChannelType.MTCH,
-  ]);
-
-  macLayer.setChannelPBR(LogicalChannelType.PCCH, 2);
-  macLayer.setChannelPBR(LogicalChannelType.BCCH, 3);
-  macLayer.setChannelPBR(LogicalChannelType.CCCH, 4);
-  macLayer.setChannelPBR(LogicalChannelType.DCCH, 1);
-  macLayer.setChannelPBR(LogicalChannelType.DTCH, 6);
-  macLayer.setChannelPBR(LogicalChannelType.MCCH, 1);
-  macLayer.setChannelPBR(LogicalChannelType.MTCH, 3);
-
-  macLayer.setMaxPDU(30);
-
-  macLayer.sendViaUpLink(LogicalChannelType.PCCH, 5);
-  macLayer.sendViaUpLink(LogicalChannelType.BCCH, 10);
-  macLayer.sendViaUpLink(LogicalChannelType.CCCH, 7);
-  macLayer.sendViaUpLink(LogicalChannelType.DCCH, 3);
-  macLayer.sendViaUpLink(LogicalChannelType.DTCH, 8);
-  macLayer.sendViaUpLink(LogicalChannelType.MCCH, 2);
-  macLayer.sendViaUpLink(LogicalChannelType.MTCH, 6);
-
-  PCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.PCCH));
-  BCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.BCCH));
-  CCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.CCCH));
-  DCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DCCH));
-  DTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DTCH));
-  MCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MCCH));
-  MTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MTCH));
-
-  PCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.PCH));
-  BCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.BCH));
-  DL_SCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.DL_SCH));
-  MCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.MCH));
-
-  simulationStep();
-}
-
-function simulationStep() {
-  if (!macLayer.isDownLinkFinished()) {
-    macLayer.doDownLinkStep();
-    PCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.PCCH));
-
-    BCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.BCCH));
-    CCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.CCCH));
-    DCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DCCH));
-    DTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DTCH));
-    MCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MCCH));
-    MTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MTCH));
-    PCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.PCH));
-
-    BCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.BCH));
-    DL_SCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.DL_SCH));
-    MCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.MCH));
-
-    const action = <TransmitPacketDownLink>macLayer.getLastAction();
-
-    const animation = new SlideAnimation(
-      new Box(30, 30, '' + action.bits),
-      new Vector2D(0, 100),
-      1000
-    );
-
-    const packetAnimation = new Transform(
-      new Vector2D(200, 110),
-      animation,
-    );
-    const id = renderer.addObject(packetAnimation);
-
-    animation.waitFor().then(() => {
-      renderer.removeObject(id);
-      simulationStep();
-    });
-  }
-}
-
-
-
-// Radio Link Control Layer
-renderer.addObject(
-  new Transform(
-    new Vector2D(5, 5),
-    new Box(390, 80)
-  )
+const simulationController = new SimulationController(
+  new Map<string, HTMLInputElement>([
+    ['PCCH', <HTMLInputElement>document.getElementById('PCCH-bits')],
+    ['BCCH', <HTMLInputElement>document.getElementById('BCCH-bits')],
+    ['CCCH', <HTMLInputElement>document.getElementById('CCCH-bits')],
+    ['DCCH', <HTMLInputElement>document.getElementById('DCCH-bits')],
+    ['DTCH', <HTMLInputElement>document.getElementById('DTCH-bits')],
+    ['MCCH', <HTMLInputElement>document.getElementById('MCCH-bits')],
+    ['MTCH', <HTMLInputElement>document.getElementById('MTCH-bits')],
+  ]),
+  new Map<LogicalChannelType, HTMLInputElement>([
+    [LogicalChannelType.PCCH, <HTMLInputElement>document.getElementById('PCCH-pbr')],
+    [LogicalChannelType.BCCH, <HTMLInputElement>document.getElementById('BCCH-pbr')],
+    [LogicalChannelType.CCCH, <HTMLInputElement>document.getElementById('CCCH-pbr')],
+    [LogicalChannelType.DCCH, <HTMLInputElement>document.getElementById('DCCH-pbr')],
+    [LogicalChannelType.DTCH, <HTMLInputElement>document.getElementById('DTCH-pbr')],
+    [LogicalChannelType.MCCH, <HTMLInputElement>document.getElementById('MCCH-pbr')],
+    [LogicalChannelType.MTCH, <HTMLInputElement>document.getElementById('MTCH-pbr')],
+  ]),
+  <HTMLInputElement>document.getElementById('PDU'),
+  simulation,
+  macLayer
 );
 
-// Mac Layer
-renderer.addObject(
-  new Transform(
-    new Vector2D(5, 135),
-    new Box(390, 80)
-  )
-);
+renderer.addObject(simulation);
 
-// Physical Layer
-renderer.addObject(
-  new Transform(
-    new Vector2D(5, 265),
-    new Box(390, 80)
-  )
-);
-
-// Physical Layer PDU
-renderer.addObject(
-  new Transform(
-    new Vector2D(100, 280),
-    new Box(200, 50)
-  )
-);
+startSimulationButton.addEventListener('click', async () => {
+  await simulationController.start();
+});
 
 
-// Channels
-const row = new RowLayout(47);
-row.addObject(PCCH);
-row.addObject(BCCH);
-row.addObject(CCCH);
-row.addObject(DCCH);
-row.addObject(DTCH);
-row.addObject(MCCH);
-row.addObject(MTCH);
-
-renderer.addObject(new Transform(
-  new Vector2D(62, 110),
-  row
-));
-
-// Channels
-const row2 = new RowLayout(60);
-row2.addObject(PCH);
-row2.addObject(BCH);
-row2.addObject(DL_SCH);
-row2.addObject(MCH);
-
-renderer.addObject(
-  new Transform(
-    new Vector2D(107, 240),
-    row2
-  )
-);
-
-// Connections
-// renderer.addObject(
-//   new Line(
-//     new Vector2D(62, 140),
-//     new Vector2D(107, 210),
-//   )
-// );
 //
-// renderer.addObject(
-//   new Line(
-//     new Vector2D(154, 140),
-//     new Vector2D(199, 210),
-//   )
-// );
+//   simulationStep();
+// }
 //
-// renderer.addObject(
-//   new Line(
-//     new Vector2D(246, 140),
-//     new Vector2D(199, 210),
-//   )
-// );
+// function simulationStep() {
+//   if (!macLayer.isDownLinkFinished()) {
+//     macLayer.doDownLinkStep();
+//     PCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.PCCH));
 //
-// renderer.addObject(
-//   new Line(
-//     new Vector2D(338, 140),
-//     new Vector2D(291, 210),
-//   )
-// );
-
+//     BCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.BCCH));
+//     CCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.CCCH));
+//     DCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DCCH));
+//     DTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DTCH));
+//     MCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MCCH));
+//     MTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MTCH));
+//     PCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.PCH));
+//
+//     BCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.BCH));
+//     DL_SCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.DL_SCH));
+//     MCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.MCH));
+//
+//     const action = <TransmitPacketDownLink>macLayer.getLastAction();
+//
+//     const animation = new SlideAnimation(
+//       new Box(30, 30, '' + action.bits),
+//       new Vector2D(0, 100),
+//       1000
+//     );
+//
+//     const packetAnimation = new Transform(
+//       new Vector2D(200, 110),
+//       animation,
+//     );
+//     const id = renderer.addObject(packetAnimation);
+//
+//     animation.waitFor().then(() => {
+//       renderer.removeObject(id);
+//       simulationStep();
+//     });
+//   }
+// }
+//
+//
 
 
 function frame() {
