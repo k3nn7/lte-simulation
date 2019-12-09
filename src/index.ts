a45053ed7d592f1153fc6149d1da0d33b6ee289a
Łukasz Lalik <lukasz.lalik@gmail.com>
2019-12-09 08:35:27 +0100

    Remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Renderer from "./engine/Renderer";
-import Transform from "./engine/Transform";
-import Vector2D from "./engine/Vector2D";
-import Box from "./renderables/Box";
-import Circle from "./renderables/Circle";
-import RowLayout from "./renderables/RowLayout";
 import {MacLayer} from "./model/lte/MacLayer";
-import {LogicalChannelType, TransportChannelType} from "./model/lte/ChannelType";
-import {SlideAnimation} from "./animators/SlideAnimation";
-import {TransmitPacketDownLink} from "./model/lte/Action";
+import {LogicalChannelType} from "./model/lte/ChannelType";
 import {Simulation} from "./view/Simulation";
 import {SimulationController} from "./controller/SimulationController";
 
@@ -18,9 +11,7 @@ const renderer = new Renderer(canvas.getContext('2d'));
 const startSimulationButton = <HTMLButtonElement>document.getElementById('start-simulation');
 
 const macLayer = new MacLayer();
-
 const simulation = new Simulation();
-
 const simulationController = new SimulationController(
   new Map<string, HTMLInputElement>([
     ['PCCH', <HTMLInputElement>document.getElementById('PCCH-bits')],
@@ -50,52 +41,6 @@ renderer.addObject(simulation);
 startSimulationButton.addEventListener('click', async () => {
   await simulationController.start();
 });
-
-
-//
-//   simulationStep();
-// }
-//
-// function simulationStep() {
-//   if (!macLayer.isDownLinkFinished()) {
-//     macLayer.doDownLinkStep();
-//     PCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.PCCH));
-//
-//     BCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.BCCH));
-//     CCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.CCCH));
-//     DCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DCCH));
-//     DTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.DTCH));
-//     MCCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MCCH));
-//     MTCH.setText('' + macLayer.peekUpLink().get(LogicalChannelType.MTCH));
-//     PCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.PCH));
-//
-//     BCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.BCH));
-//     DL_SCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.DL_SCH));
-//     MCH.setText('' + macLayer.peekDownLink().get(TransportChannelType.MCH));
-//
-//     const action = <TransmitPacketDownLink>macLayer.getLastAction();
-//
-//     const animation = new SlideAnimation(
-//       new Box(30, 30, '' + action.bits),
-//       new Vector2D(0, 100),
-//       1000
-//     );
-//
-//     const packetAnimation = new Transform(
-//       new Vector2D(200, 110),
-//       animation,
-//     );
-//     const id = renderer.addObject(packetAnimation);
-//
-//     animation.waitFor().then(() => {
-//       renderer.removeObject(id);
-//       simulationStep();
-//     });
-//   }
-// }
-//
-//
-
 
 function frame() {
   renderer.render();
