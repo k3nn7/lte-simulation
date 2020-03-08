23c8569e4edcd028565932c2d4c2888a81bc246c
Łukasz Lalik <lukasz.lalik@brainly.com>
2020-03-08 19:40:05 +0100

    Define colors palette

---
+++ src/colors.js
@@ -0,0 +1,8 @@
+export const BG_DARK_2 = 0xC43C00;
+export const BG_MEDIUM_2 = 0xFF6D00;
+export const FG_2 = 0xFFFFFF;
+
+export const BG_LIGHT_1 = 0xFFFFFF;
+export const FG_1 = 0x000000;
+
+export const BG_MEDIUM_3 = 0x37474F;
