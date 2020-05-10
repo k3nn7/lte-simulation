eae0a5d7068cc7106a6840a59aab0cb4543eb17d
Łukasz Lalik <lukasz.lalik@brainly.com>
2020-05-10 12:29:39 +0200

    Use Connectable in KneeConnector

--- src/layer/kneeConnector.js
+++ src/layer/kneeConnector.js
@@ -1,29 +1,59 @@
 import * as PIXI from 'pixi.js';
-import {BG_MEDIUM_2} from "../colors";
+import Connectable from 'Common/connectable';
+import {BG_MEDIUM_2} from 'Common/colors';
+import {moveToThePoint, spit, swallow} from "../utils";
 
-export default class KneeConnector extends PIXI.Graphics {
-  constructor(from, to) {
+export default class KneeConnector extends Connectable {
+  constructor(componentA, componentB) {
     super();
 
+    const pointA = new PIXI.Point(
+      componentA.x + (componentA.width / 2),
+      componentA.y + componentA.height
+    );
+    const pointB = new PIXI.Point(
+      pointA.x,
+      pointA.y + 50
+    );
+    const pointC = new PIXI.Point(
+      componentB.x + (componentB.width / 2) + 600,
+      pointB.y
+    );
+    const pointD = new PIXI.Point(
+      pointC.x,
+      componentB.y + componentB.height
+    );
+
     this.lineStyle(3, BG_MEDIUM_2);
-    this.moveTo(
-      from.x + (from.width / 2),
-      from.y + from.height
-    );
+    this.position = pointA;
+    this.lineTo(pointB.x - pointA.x, pointB.y - pointA.y);
+    this.lineTo(pointC.x - pointA.x, pointC.y - pointA.y);
+    this.lineTo(pointD.x - pointA.x, pointD.y - pointA.y);
 
-    this.lineTo(
-      from.x + (from.width / 2),
-      from.y + from.height + 50
-    );
+    componentA.setChannelB((data) => this.onChannelA(data));
 
-    this.lineTo(
-      to.x + (to.width / 2) + 600,
-      to.y + to.height + 50
-    );
+    this.setChannelB((data) => componentB.onChannelB(data));
+    this.setChannelA((data) => componentA.onChannelB(data));
 
-    this.lineTo(
-      to.x + (to.width / 2) + 600,
-      to.y + to.height
-    );
+    this.channelAPosition = new PIXI.Point(0, 0);
+    this.knee1Position = new PIXI.Point(pointB.x - pointA.x, pointB.y - pointA.y);
+    this.knee2Position = new PIXI.Point(pointC.x - pointA.x, pointC.y - pointA.y);
+    this.channelBPosition = new PIXI.Point(pointD.x - pointA.x, pointD.y - pointA.y);
+  }
+
+  async onChannelA(data) {
+    this.addChild(data);
+    data.scale.set(0, 0);
+    data.position = this.channelAPosition;
+    await spit(data, 200);
+    data.activate();
+
+    await moveToThePoint(data, this.knee1Position, 500);
+    await moveToThePoint(data, this.knee2Position, 500);
+    await moveToThePoint(data, this.channelBPosition, 500);
+    await swallow(data, 200);
+    data.stop();
+    this.removeChild(data);
+    this.channelB(data);
   }
 }
