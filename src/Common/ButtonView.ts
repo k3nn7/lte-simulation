9a26280078be1445fa085805e592c216cad1d83d
Łukasz Lalik <lukasz.lalik@brainly.com>
2020-06-15 16:07:13 +0200

    Add ButtonView common element

---
+++ src/Common/ButtonView.ts
@@ -0,0 +1,33 @@
+import * as PIXI from 'pixi.js';
+import {BG_MEDIUM_2, FG_2} from './colors';
+
+export default class ButtonView extends PIXI.Graphics {
+  onClick: () => void;
+
+  constructor(caption: string) {
+    super();
+
+    this.beginFill(BG_MEDIUM_2);
+    this.drawRoundedRect(0, 0, 120, 30, 10);
+    this.endFill();
+
+    this.interactive = true;
+    this.buttonMode = true;
+    this.on('click', () => {
+      if (this.onClick) this.onClick();
+    })
+
+    const captionText = new PIXI.Text(caption, {
+      fill: FG_2,
+      fontSize: 15,
+      align: 'center'
+    });
+    captionText.position.set(this.width / 2, this.height / 2);
+    captionText.anchor.set(0.5, 0.5);
+    this.addChild(captionText);
+  }
+
+  setOnClick(onClick: () => void) {
+    this.onClick = onClick;
+  }
+}
