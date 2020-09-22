dd4d490af35185e5c35d37c0fcfc4f2b5398deec
Łukasz Lalik <lukasz.lalik@gmail.com>
2020-09-22 11:12:19 +0200

    Add debug mode to UE RLC sublayer

--- src/UE/RLC/RLCView.ts
+++ src/UE/RLC/RLCView.ts
@@ -3,25 +3,35 @@ import BufferItemView from 'Common/BufferItemView';
 import LayerView from 'Common/LayerView';
 import ReceptionBufferView from "./receptionBufferView";
 import {DataUnit} from "../../Common/DataUnit";
-import FlatSDU from "../../eNB/RLC/FlatSDU";
 import {PDCPDataUnit} from "../../Common/DataUnit/PDCPDataUnit";
+import ButtonView from "../../Common/ButtonView";
+import {IPPacket} from "../../Common/IP/IPPacket";
+import MinimizedPacket from "../../eNB/RLC/MinimizedPacket";
+import {PDCPAck} from "../../Common/DataUnit/PDCPAck";
 
 export default class RLCView extends LayerView {
-  receptionBuffer: ReceptionBufferView
+  receptionBuffer: ReceptionBufferView;
+  sendPacketButton: ButtonView;
 
-  constructor(resources: Partial<Record<string, PIXI.LoaderResource>>) {
+  constructor(resources: Partial<Record<string, PIXI.LoaderResource>>, debugMode: boolean) {
     super(resources, 'RLC');
 
     this.receptionBuffer = new ReceptionBufferView(resources);
     this.receptionBuffer.position.set(10, 10);
     this.body.addChild(this.receptionBuffer);
 
+    this.initDebugMode(debugMode);
+
     setTimeout(() => this.forwardSDU(), 5000);
   }
 
   async onChannelB(data: DataUnit) {
-    const bufferItem = new BufferItemView(data, 'SDU');
-    await this.receptionBuffer.addItem(bufferItem);
+    if (data instanceof MinimizedPacket) {
+      const bufferItem = new BufferItemView(data, 'SDU');
+      await this.receptionBuffer.addItem(bufferItem);
+
+      this.channelB.sendOut(new PDCPAck(data.packets[0], 1));
+    }
   }
 
   async forwardSDU() {
@@ -29,10 +39,29 @@ export default class RLCView extends LayerView {
       const item = await this.receptionBuffer.popItem();
 
       item.wrappedItem.packets.forEach((pdcpDataUnit: PDCPDataUnit) => {
-        this.channelA(pdcpDataUnit);
+        this.channelA.sendOut(pdcpDataUnit);
       });
     }
 
     setTimeout(() => this.forwardSDU(), 5000);
+  }
+
+  private initDebugMode(debugMode: boolean) {
+    if (!debugMode) return;
+
+    this.sendPacketButton = new ButtonView('+', 20, 20);
+    this.sendPacketButton.setOnClick(() => {
+      this.onChannelB(
+        new MinimizedPacket(
+          50,
+          [new PDCPDataUnit(
+            new IPPacket('foooo sdafasdfasdfasdfasdfasdf dsfafdsfasfasdfa', 10),
+            1
+          )]
+        )
+      )
+    });
+    this.sendPacketButton.position.set(5, 120);
+    this.addChild(this.sendPacketButton);
   }
 }
