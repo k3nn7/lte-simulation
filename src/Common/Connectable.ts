ce246c43780daa4b6e6a0ccc2b254699446e6f44
Łukasz Lalik <lukasz.lalik@brainly.com>
2020-10-15 21:22:31 +0200

    Make Connectable an abstract class

--- src/Common/Connectable.ts
+++ src/Common/Connectable.ts
@@ -3,15 +3,15 @@ import {DataUnit} from "./DataUnit";
 
 export type Channel = (data: DataUnit) => Promise<void>
 
-export default class Connectable extends PIXI.Graphics {
+export default abstract class Connectable extends PIXI.Graphics {
   channelA: Channel;
   channelB: Channel;
 
-  setChannelA(channel: Channel) {
+  public setChannelA(channel: Channel) {
     this.channelA = channel;
   }
 
-  setChannelB(channel: Channel) {
+  public setChannelB(channel: Channel) {
     this.channelB = channel;
   }
 
